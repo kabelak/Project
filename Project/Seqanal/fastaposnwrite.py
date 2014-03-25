1a85f78a27e0d32c8243558b6f0cd5bb1e30eec6
Kavin <kabelak@users.noreply.github.com>
2014-03-25 13:19:11 +0000

    Fixed problem with ID - now full fasta description will appear (up to 90 characters).

--- Project/Seqanal/fastaposnwrite.py
+++ Project/Seqanal/fastaposnwrite.py
@@ -6,5 +6,5 @@ from Bio import SeqIO
 
 with open("outfile.txt","w") as f:
         for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
-                f.write(">" + str(seq_record.id) + str(seq_record.name) + "\n")
-                f.write(str(seq_record.seq[-70:-20]) + "\n") # position -30 to -70
+                f.write(">" + str(seq_record.description[:90]) + "\n")
+                f.write(str(seq_record.seq[-70:-20]) + "\n\n") # position -30 to -70
