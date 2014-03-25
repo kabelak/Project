88eb6f7730a9216d8023a9b0f6b4e92d94b3b33c
Kavin <kabelak@users.noreply.github.com>
2014-03-25 13:31:03 +0000

    description too long with ">" for locARNA - so changed to 89

--- Project/Seqanal/fastaposnwrite.py
+++ Project/Seqanal/fastaposnwrite.py
@@ -6,5 +6,5 @@ from Bio import SeqIO
 
 with open("outfile.txt","w") as f:
         for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
-                f.write(">" + str(seq_record.description[:90]) + "\n")
+                f.write(">" + str(seq_record.description[:89]) + "\n")
                 f.write(str(seq_record.seq[-65:-5]) + "\n\n") # position -5 to -65
