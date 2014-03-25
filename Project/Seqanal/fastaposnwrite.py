f93833e0346ca605b77f079b6bc2f6a63db8141e
Kavin <kabelak@users.noreply.github.com>
2014-03-25 13:25:20 +0000

    changed positions as some parts were being missed

--- Project/Seqanal/fastaposnwrite.py
+++ Project/Seqanal/fastaposnwrite.py
@@ -7,4 +7,4 @@ from Bio import SeqIO
 with open("outfile.txt","w") as f:
         for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
                 f.write(">" + str(seq_record.description[:90]) + "\n")
-                f.write(str(seq_record.seq[-70:-20]) + "\n\n") # position -30 to -70
+                f.write(str(seq_record.seq[-65:-5]) + "\n\n") # position -5 to -65
