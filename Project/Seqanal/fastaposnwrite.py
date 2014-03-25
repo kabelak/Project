89cf9b90381b69a73f5a5ea005bbf817854cdd17
Kavin <kabelak@users.noreply.github.com>
2014-03-25 13:15:54 +0000

    made the output fasta like and added the name of the fasta seq, not just ID

--- Project/Seqanal/fastaposnwrite.py
+++ Project/Seqanal/fastaposnwrite.py
@@ -1,9 +1,10 @@
 __author__ = 'Kavin'
 
+import sys
 
 from Bio import SeqIO
 
 with open("outfile.txt","w") as f:
         for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
-                f.write(str(seq_record.id) + "\n")
+                f.write(">" + str(seq_record.id) + str(seq_record.name) + "\n")
                 f.write(str(seq_record.seq[-70:-20]) + "\n") # position -30 to -70
