e8a769d49af82d549acb5605cf5279878bd915bd
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-04-23 15:32:24 +0100

    Added comments Changed names of dataframe header changed data to be put in df to value instead of mean

--- R/Code/compdata.R
+++ R/Code/compdata.R
@@ -1,18 +1,21 @@
 compdata = function(data1, data2){
 	N = nrow(data1)
 	N2 = nrow(data2)
-	if (N2 < N){ N = N2 }
-	i = 1
+	
+  # Prevent EOF errors by limiting to lowest row number
+  if (N2 < N){ N = N2 }
+	
+	# create a dataframe to store data
+	result.df = data.frame(t(rep(NA,3)))
+	names(result.df) = c("Nuc_Posn", "Value1", "Value2")
 
 	# set a window of allowable difference
 	w = 20
-
-	# create a dataframe to store data
-	result.df = data.frame(t(rep(NA,3)))
-	names(result.df) = c("Nucleotide", "Mean1", "Mean2")
-
+	
+	i = 1
 	while ((i+50) <= N){
     
+    # Ignore low values
     while ((data1$For[i] <= 5) && (data2$For[i] <= 5)){
       i = i+1
     }
@@ -20,8 +23,9 @@ compdata = function(data1, data2){
 		mean1 = mean(data1$For[i:(i+50)])
 		mean2 = mean(data2$For[i:(i+50)])
 		if (mean1 < (mean2-w) || mean1 > (mean2+w)){
-			result.df = rbind(result.df,c(i, mean1, mean2))
+			result.df = rbind(result.df,c(i, data1$For[i], data2$For[i]))
 		}
+    
 		i = i + 10
 	}
 
