6100e25e93d1d480a262316e139635583037aa8b
Kavin <kabelak@users.noreply.github.com>
2014-04-23 10:05:12 +0100

    Changed initial i to 1 as R starts from 1

--- R/Code/compdata.R
+++ R/Code/compdata.R
@@ -2,7 +2,7 @@ compdata = function(data1, data2){
 	N = nrow(data1)
 	N2 = nrow(data2)
 	if (N2 < N){ N = N2 }
-	i = 0
+	i = 1
 
 	# set a window of allowable difference
 	w = 20
@@ -12,6 +12,11 @@ compdata = function(data1, data2){
 	names(result.df) = c("Nucleotide", "Mean1", "Mean2")
 
 	while ((i+50) <= N){
+    
+    while ((data1$For[i] <= 5) && (data2$For[i] <= 5)){
+      i = i+1
+    }
+    
 		mean1 = mean(data1$For[i:(i+50)])
 		mean2 = mean(data2$For[i:(i+50)])
 		if (mean1 < (mean2-w) || mean1 > (mean2+w)){
@@ -26,7 +31,8 @@ compdata = function(data1, data2){
 	return(result.df)
 }
 
-
+# Instead of mean, it may be a better idea to look at gradient - if the gradient is considerably larger (more positive), it will show that a feature is starting, and that it will rise higher than the correspoding feature, if present, in the other data
 # Function to create empty df with colnames
 # nodata <- as.data.frame(setNames(replicate(5,numeric(0), simplify = F), letters[1:5]))
+
 
