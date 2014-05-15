b574a3a87ab99f6495832b66924cb13f7a4be4bc
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-05-15 23:48:32 +0100

    now using dict of dicts

--- Project/Seqanal/extractmatch2.py
+++ Project/Seqanal/extractmatch2.py
@@ -5,7 +5,7 @@ import sys
 
 f = open(sys.argv[1], 'rU')
 
-matches1 = [] # make this a dict {}
+matches1 = {} # make this a dict {}
 matches = {}
 
 # f.readline() until f.readline().startswith('Match')
@@ -17,14 +17,24 @@ for line in f:
         line = next(f)
         while not line.startswith('None'):
             line = re.sub('\n|\t', '', line)
+            characteristic = re.search('(.*):\s(.*)', line)
             # Re match before : match after
             # matches1[group1] = group 2
-            matches1.append(line) ## delete this
+            #matches1.append(line) ## delete this
+            matches1[characteristic.group(1)] = characteristic.group(2)
             line = next(f)
         matches[entry] = matches1
-        matches1= []
+        matches1= {}
 
-print matches
+for key, value in matches.items():
+    position = re.search('\((\d*):(\d*)', key)
+    print value['Sequence'], 'starts at ', position.group(1), ' and ends at ', position.group(2)
+    print 'This is part of gene/feature ', value['Feature']
+
+
+    #print key, value['Direction']
+
+
 
 # case: what about matches where there are sequences upstream and downstream? -- duplicate entry up to certain point, then replace dict values
 # rule 1: matches between TSS and start codon - read from other file
