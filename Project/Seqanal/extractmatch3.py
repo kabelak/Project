87de12d515184ff1e9099cd87faf7dd4eb5a072d
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-05-15 18:21:27 +0100

    match3 is the one with the tuple. match2 is now with dict

--- Project/Seqanal/extractmatch3.py
+++ Project/Seqanal/extractmatch3.py
@@ -5,27 +5,24 @@ import sys
 
 f = open(sys.argv[1], 'rU')
 
-matches1 = [] # make this a dict {}
-matches = {}
+matches1 = []
+matches = ()
 
 # f.readline() until f.readline().startswith('Match')
 
 for line in f:
     if line.startswith('Match'):
         line = re.sub('\n', '', line)
-        entry = line
+        matches1.append(line)
         line = next(f)
         while not line.startswith('None'):
             line = re.sub('\n|\t', '', line)
-            # Re match before : match after
-            # matches1[group1] = group 2
-            matches1.append(line) ## delete this
+            matches1.append(line)
             line = next(f)
-        matches[entry] = matches1
+        matches = matches + (matches1,)
         matches1= []
 
 print matches
 
-# case: what about matches where there are sequences upstream and downstream? -- duplicate entry up to certain point, then replace dict values
 # rule 1: matches between TSS and start codon - read from other file
 # rule 2: remove ones which do not fold nicely
