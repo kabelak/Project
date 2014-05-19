69778355a6084f949d4d5627f2ccb1367ad30752
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-05-19 23:43:49 +0100

    working code

--- Project/Seqanal/blast.py
+++ Project/Seqanal/blast.py
@@ -8,11 +8,11 @@ with open(sys.argv[1], "rU") as f:
     for line in f:
         if line.startswith('>'):
             print "\n"
-            org = re.search("gi\|(\d*)\|.*?\[(\w*)\s?(\w*)?.*?\]", line)
+            org = re.search("gi\|(\d*)\|.*?\[(.*)\]", line)
             #print org.group(1), org.group(2), org.group(3)
             #if org.group(3):
             #print ">",org.group(2),"_",org.group(3),org.group(1)
-            print ">",org.group(2),"_",org.group(3),org.group(1)
+            print ">"+re.sub('\s', '_', org.group(2))+"||"+org.group(1)
             #else:
             #    print ">", org.group(2), org.group(1)
         else:
