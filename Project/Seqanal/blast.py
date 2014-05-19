074ea1cb62b47ab7f44e69bba82f67ed20c33bca
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-05-19 23:35:09 +0100

    initial commit

---
+++ Project/Seqanal/blast.py
@@ -0,0 +1,22 @@
+__author__ = 'Kavin'
+
+
+import re
+import sys
+
+with open(sys.argv[1], "rU") as f:
+    for line in f:
+        if line.startswith('>'):
+            print "\n"
+            org = re.search("gi\|(\d*)\|.*?\[(\w*)\s?(\w*)?.*?\]", line)
+            #print org.group(1), org.group(2), org.group(3)
+            #if org.group(3):
+            #print ">",org.group(2),"_",org.group(3),org.group(1)
+            print ">",org.group(2),"_",org.group(3),org.group(1)
+            #else:
+            #    print ">", org.group(2), org.group(1)
+        else:
+            line = re.sub('\n', '', line)
+            print line
+
+
