1da6b71b01c28cad9dafdc1ef9c21d702ccdaea7
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-05-21 13:09:36 +0100

    changed to replace all spaces by _

---
+++ Project/Seqanal/fastaposnwritebad.py
@@ -0,0 +1,13 @@
+__author__ = 'Kavin'
+
+import sys
+import re
+
+from Bio import SeqIO
+
+with open("outfile.txt","w") as f:
+        for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
+            org = re.search('\[(.*)\]', str(seq_record.description))
+            #print org.group(1)
+            f.write(">" + str(seq_record.description[:13]) + org.group(1) + "\n")
+            SeqIO.write(seq_record.seq, f, "fasta") # position -5 to -65
