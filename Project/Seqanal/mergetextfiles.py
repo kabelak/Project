155cab1c75ea16f9dbf2b38222000e698443d31f
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-05-21 12:52:11 +0100

    update to exclude output file (merge.txt) and python file (mergetextfiles.py) from being written into the output

--- Project/Seqanal/mergetextfiles.py
+++ Project/Seqanal/mergetextfiles.py
@@ -4,15 +4,18 @@ __author__ = 'Kavin'
 import os
 import re
 
+output = open("merge.txt", "w")
+
 filenames = os.listdir('.')
 for f in filenames:
+    if f != "merge.txt" and f != "mergetextfiles.py":
         with open(f, 'r') as content_file:
             content = content_file.read()
-            print content
-            print '\n'
+            output.write(content + '\n')
 
 
 
+output.close()
 
 
 
