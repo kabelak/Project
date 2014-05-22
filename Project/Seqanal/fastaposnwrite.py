3d9ea6287bb824634fccf55327a2675c4c2104dd
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-05-22 23:04:20 +0100

    changed to produce output filename with same filename as input + _processed

--- Project/Seqanal/fastaposnwrite.py
+++ Project/Seqanal/fastaposnwrite.py
@@ -1,10 +1,16 @@
 __author__ = 'Kavin'
 
 import sys
+import re
+
 
 from Bio import SeqIO
 
-with open("outRNAfastafile.txt","w") as f:
+fname = re.search('(.*)\.\w*', sys.argv[1])
+fname2 = str(fname.group(1))+'_processed.txt'
+
+with open(fname2,"w") as f:
         for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
-                f.write(">" + str(seq_record.description[4:12]) + seq_record.description[43:81] + "\n")
+                string = ">" + str(seq_record.description[4:12]) + str(seq_record.description[43:81])
+                f.write(re.sub('\s|\|', '_', string) + "\n")
                 f.write(str(seq_record.seq) + "\n\n")
