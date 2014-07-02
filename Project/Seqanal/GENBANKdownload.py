8ada3485550fb1e592ba50e5d46ab08883a0f3c7
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-07-02 16:26:18 +0100

    made input name and output file name generic based on an input string

--- Project/Seqanal/GENBANKdownload.py
+++ Project/Seqanal/GENBANKdownload.py
@@ -4,12 +4,14 @@ from Bio import Entrez
 
 # ref.: http://wilke.openwetware.org/Parsing_Genbank_files_with_Biopython.html
 
+organism_id = 'HE804045.1'
+
 # replace with your real email (optional):
 Entrez.email = 'whatever@mail.com'
 # accession id works, returns genbank format, looks in the 'nucleotide' database:
-handle=Entrez.efetch(db='nucleotide',id='CP002059.1',rettype='gb')
+handle=Entrez.efetch(db='nucleotide',id=organism_id,rettype='gb')
 # store locally:
-local_file=open('CP002059.1.gb', 'w')
+local_file=open(str(organism_id+'.gb'), 'w')
 local_file.write(handle.read())
 handle.close()
 local_file.close()
