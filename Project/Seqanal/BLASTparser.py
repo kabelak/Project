c53455fa8c6634ade375459731b3f1be41a342fe
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-07-02 12:17:12 +0100

    Added more objects

--- Project/Seqanal/BLASTparser.py
+++ Project/Seqanal/BLASTparser.py
@@ -18,12 +18,21 @@ with open(sys.argv[1], "rU") as fh:
             for hsp in alignment.hsps:
                 if hsp.expect < 0.04:
                     print('****Alignment****')
-                    print('sequence:', alignment.title)
+                    #print('sequence:', alignment.title)
+                    print('Hit ID:', alignment.hit_id)
+                    print('Hit def:', alignment.hit_def)
                     print('length:', alignment.length)
+                    print('align length:', hsp.align_length)
+                    print('hit start:', hsp.sbjct_start)
+                    print('hit end:', hsp.sbjct_end)
+                    print('query start:', hsp.query_start)
+                    print('query end:', hsp.query_end)
+                    print('strand:', hsp.frame)
                     print('e value:', hsp.expect)
-                    print(hsp.query[0:75] + '...')
-                    print(hsp.match[0:75] + '...')
-                    print(hsp.sbjct[0:75] + '...')
+                    print('identities:', hsp.identities)
+                    print(hsp.query[:50] + '...')
+                    print(hsp.match[:50] + '...')
+                    print(hsp.sbjct[:50] + '...')
 
 
 
