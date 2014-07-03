40f8d65b88a5c3ee1146797e120ec6b378994513
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-07-03 16:26:23 +0100

    If BLAST result strand and genbank match strand are different, program will ammend to product

--- Project/Seqanal/Blast Parser GB downloader/BlaParGenBaDowSeqEx.py
+++ Project/Seqanal/Blast Parser GB downloader/BlaParGenBaDowSeqEx.py
@@ -37,6 +37,7 @@ def GenBankParser(gbFile, start, frame, IRE=200):
                     # TODO how to make sure it is checking in the right strand? At the moment, the 'start in feature' seems to find... but how?
                     if frame != location.strand:
                         print '!!!!!Strands do not match! The GBrecord strand is', location.strand, 'while the BLAST strand is', frame, '!!!!!!!!'
+                        product = str(product + 'DIFFERENT STRANDS')
                     if location.strand == 1:
                         ire_start = location.start.position-IRE
                         ire_end = location.start.position
