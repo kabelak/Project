dacf92c7984ec5ff8181441699d361b8a4d59d81
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-07-07 17:10:56 +0100

    Added input excel file from terminal prompt Embelish

--- Project/Seqanal/extractmatch2.py
+++ Project/Seqanal/extractmatch2.py
@@ -1,14 +1,24 @@
 __author__ = 'Kavin'
 
+########################################################################################################
+# Script to match entries within a Spire output and TSS/Startcodon entries within an Excel Spreadsheet #
+# Usage:                                                                                               #
+#   python extractmatch2.py [Spire Output File] [Excel Sheet]                                          #
+# Output:                                                                                              #
+#   Will display Rv number of match, RNA sequence of Spire match and start/end positions               #
+########################################################################################################
+
 import re
 import sys
 import xlrd
 
-
-## Import data from Spire output file
-
+#### Import data from Spire output file
+# Create dicts to score Spire output data
 matches1 = {}
 matches = {}
+
+# For each entry within Spire output (ie, each match), create a temp dict 'matches1' with all the charateristics, then
+# create a glocal dict key for the entry with the characteristics as a value (ie, dict of dicts)
 with open(sys.argv[1], 'rU') as f:
   for line in f:
     if line.startswith('Match'):
@@ -27,56 +37,55 @@ with open(sys.argv[1], 'rU') as f:
         matches1= {}
 
 
-## Import data from Excel workbook/sheet
-workbook = xlrd.open_workbook("F:\Google Drive\Birkbeck\Project\RNAseq\Cortes sup mat\mmc3TSSvsStartCodon.xlsx")
+
+#### Import data from Excel workbook/sheet
+#workbook = xlrd.open_workbook("F:\Google Drive\Birkbeck\Project\RNAseq\Cortes sup mat\mmc3TSSvsStartCodon.xlsx")
+workbook = xlrd.open_workbook(sys.argv[2])
 sheet = workbook.sheet_by_index(0)
 data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
 
+# Create dicts to store TSS data
 TSS = {}
 row = {}
-for i in range(2, 15): #sheet.nrows
+
+# For each entry (row), store the startcodon and TSS as key/values within the value of a global dict
+# with gene name as key
+for i in range(2, 15):
     # print i
     Gene = data[i][0]
     # print Gene
     row['StartCodon'] = data[i][4]
     if data[i][6] == '':
-        # if empty as well, go to next iteration
+        # If empty as well, go to next iteration
         row['TSS'] = data[i][9]
     else:
         row['TSS'] = data[i][6]
     TSS[Gene] = row
     row = {}
 
-#print TSS
 
+#### Find intersection
+# For each gene within the Excel sheet, iterate through entries within Spire output to find a match
 for UTR, area in TSS.items():
     for key, value in matches.items():
         position = re.search('\((\d*):(\d*)', key)
         gene = re.search('term=(\w*)', value['URL'])
         if UTR == gene.group(1):
-            print 'Eureka!'
+            print 'Eureka! Rv number matches found!'
             print UTR, gene.group(1)
             print value['Sequence'], 'starts at ', position.group(1), ' and ends at ', position.group(2)
             print "Untrans Region starts at", area['TSS'], 'and ends at', area['StartCodon']
 
-
-
-
-
-
-
-    #print key, value['Direction']
+            #print key, value['Direction']
 
 '''
-for key, value in TSS.items():
-    if key == 'Rv0005':
-        print 'well done!', key, value
-
 for key, value in matches.items():
     position = re.search('\((\d*):(\d*)', key)
     gene = re.search('term=(\w*)', value['URL'])
     print value['Sequence'], 'starts at ', position.group(1), ' and ends at ', position.group(2)
     print 'This is part of gene ', gene.group(1), 'which is part of feature ', value['Feature']
+
+#sheet.nrows
 
 #with open(sys.argv[2], 'rU') as f2:
 # open excel file - requires either csv.read or excel package <- latter may be best for future use
