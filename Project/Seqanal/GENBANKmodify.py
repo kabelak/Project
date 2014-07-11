9458d8d3d378a316470c64e7da7767469b83c4f8
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-07-11 18:05:36 +0100

    Working code

--- Project/Seqanal/GENBANKmodify.py
+++ Project/Seqanal/GENBANKmodify.py
@@ -1,32 +1,50 @@
 __author__ = 'Kavin'
 
 from Bio import SeqIO
+from Bio import SeqFeature
+import xlrd
+
+workbook = xlrd.open_workbook("F:\Google Drive\Birkbeck\Project\RNAseq\Cortes sup mat\mmc3TSSvsStartCodon.xlsx")
+# workbook = xlrd.open_workbook(sys.argv[2])
+sheet = workbook.sheet_by_index(0)
+data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+
+# Create dicts to store TSS data
+TSS = {}
+row = {}
+# For each entry (row), store the startcodon and strand information
+for i in range(2, sheet.nrows - 1):
+    Gene = data[i][0]
+    row['Direction'] = str(data[i][3])
+    # print Gene
+    if data[i][5] < -0.7:
+        row['StartCodon'] = int(data[i][4])
+    else:
+        row['StartCodon'] = int(data[i][2])
+    TSS[str(Gene)] = row
+    row = {}
 
 gb_file = "mtbtomod.gb"
 gb_record = SeqIO.parse(open(gb_file, "r+"), "genbank")
-mysnp = 2421243
-rvnumber = 'Rv0005'
-newstart = 5357
 
 final_features = []
+# TODO look in the TSS files and include, for each gene, the closest TSS which isn't part of another gene
 
 for record in gb_record:
     for feature in record.features:
-        # print feature
-        if feature.type == "gene":
-            #print feature.type
-            if feature.qualifiers["locus_tag"][0] == rvnumber:
-                #print feature
-                print "no"
+        if feature.type == "gene" or feature.type == "CDS":
+            if TSS.has_key(feature.qualifiers["locus_tag"][0]):
+                newstart = TSS[feature.qualifiers["locus_tag"][0]]['StartCodon']
                 if feature.location.strand == 1:
-                    print "no"
-                    #feature._loc = _loc("5357..7267", 1000, 1)
-                    #feature.qualifiers["amend_position"] = "%s:%s" % (newstart, feature.location.end+1)
-                    feature._set_location_operator(5357)
-                    #print feature.location.start
+                    feature.location = SeqFeature.FeatureLocation(SeqFeature.ExactPosition(newstart - 1),
+                                                                  SeqFeature.ExactPosition(
+                                                                      feature.location.end.position),
+                                                                  feature.location.strand)
                 else:
-                    feature.location.end.position = newstart
-        final_features.append(feature)
+                    feature.location = SeqFeature.FeatureLocation(
+                        SeqFeature.ExactPosition(feature.location.start.position),
+                        SeqFeature.ExactPosition(newstart), feature.location.strand)
+        final_features.append(feature)  # Append final features
     record.features = final_features
     with open("testest.gb", "w") as for_rast:
         SeqIO.write(record, for_rast, "genbank")
