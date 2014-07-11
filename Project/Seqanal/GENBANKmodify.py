e22072333eb329b16af2a16ef48a94a72a1400cb
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-07-11 19:12:36 +0100

    embelish

--- Project/Seqanal/GENBANKmodify.py
+++ Project/Seqanal/GENBANKmodify.py
@@ -3,6 +3,8 @@ __author__ = 'Kavin'
 from Bio import SeqIO
 from Bio import SeqFeature
 import xlrd
+import sys
+import re
 
 workbook = xlrd.open_workbook("F:\Google Drive\Birkbeck\Project\RNAseq\Cortes sup mat\mmc3TSSvsStartCodon.xlsx")
 # workbook = xlrd.open_workbook(sys.argv[2])
@@ -14,23 +16,22 @@ TSS = {}
 row = {}
 # For each entry (row), store the startcodon and strand information
 for i in range(2, sheet.nrows - 1):
-    Gene = data[i][0]
-    row['Direction'] = str(data[i][3])
-    # print Gene
-    if data[i][5] < -0.7:
+    if data[i][5] < -0.7:  # Ensures BASS score is within significant range
+        Gene = data[i][0]
+        row['Direction'] = str(data[i][3])
         row['StartCodon'] = int(data[i][4])
+        TSS[str(Gene)] = row
+        row = {}
     else:
-        row['StartCodon'] = int(data[i][2])
-    TSS[str(Gene)] = row
-    row = {}
+        i += 1
 
-gb_file = "mtbtomod.gb"
-gb_record = SeqIO.parse(open(gb_file, "r+"), "genbank")
+outfile_init = re.search('(.*)\.(\w*)', sys.argv[1])
+outfile = str(outfile_init.group(1)) + '_modified.' + str(outfile_init.group(2))
 
 final_features = []
 # TODO look in the TSS files and include, for each gene, the closest TSS which isn't part of another gene
 
-for record in gb_record:
+for record in SeqIO.parse(open(sys.argv[1], "r+"), "genbank"):
     for feature in record.features:
         if feature.type == "gene" or feature.type == "CDS":
             if TSS.has_key(feature.qualifiers["locus_tag"][0]):
@@ -46,8 +47,8 @@ for record in gb_record:
                         SeqFeature.ExactPosition(newstart), feature.location.strand)
         final_features.append(feature)  # Append final features
     record.features = final_features
-    with open("testest.gb", "w") as for_rast:
-        SeqIO.write(record, for_rast, "genbank")
+    with open(outfile, "w") as new_gb:
+        SeqIO.write(record, new_gb, "genbank")
 
 '''
         if mysnp in feature:
