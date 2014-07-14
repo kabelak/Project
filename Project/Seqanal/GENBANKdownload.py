fce7ee7a48a536959cd5063e9383b34d66399bd1
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-07-14 14:27:47 +0100

    Rewrote into a nice function

--- Project/Seqanal/GENBANKdownload.py
+++ Project/Seqanal/GENBANKdownload.py
@@ -1,29 +1,20 @@
 __author__ = 'Kavin'
 
 from Bio import Entrez
+import sys
 
 # ref.: http://wilke.openwetware.org/Parsing_Genbank_files_with_Biopython.html
 
-organism_id = 'HE804045.1'
-
-# replace with your real email (optional):
-Entrez.email = 'kabela01@mail.bbk.ac.uk'
-# accession id works, returns genbank format, looks in the 'nucleotide' database:
-handle=Entrez.efetch(db='nucleotide',id=organism_id,rettype='gb')
-# store locally:
-local_file=open(str(organism_id+'.gb'), 'w')
-local_file.write(handle.read())
-handle.close()
-local_file.close()
-
-'''
+# organism_id = 'HE804045.1'
 
 def GenBankDownload(organism_id):
     Entrez.email = 'kabela01@mail.bbk.ac.uk'
-    handle = Entrez.efetch(db='nucleotide',id=organism_id,rettype='gb')
-    local_file = open(str(organism_id+'.gb'), 'w')
+    handle = Entrez.efetch(db='nucleotide', id=organism_id, rettype='gb')
+    local_file = open(str(organism_id + '.gb'), 'w')
     local_file.write(handle.read())
     handle.close()
     local_file.close()
 
-'''
+
+if __name__ == "__main__":
+    GenBankDownload(sys.argv[1])
