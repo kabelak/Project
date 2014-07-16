ab939b2b7f4354a75bae75490ba91b990c65909f
kabela01@mail.bbk.ac.uk <kabelak@users.noreply.github.com>
2014-07-16 19:23:37 +0100

    works for upstream

--- Project/Seqanal/SPIREmatch.py
+++ Project/Seqanal/SPIREmatch.py
@@ -28,13 +28,13 @@ rgrow = []
 farrest = []
 rarrest = []
 
-for i in range(5, len(datagrow)):  # replace 20 with len(datagrow)
+for i in range(5, len(datagrow)):
     if datagrow[i][1] == 'F':
         fgrow.append(int(datagrow[i][0]))
     elif datagrow[i][1] == 'R':
         rgrow.append(int(datagrow[i][0]))
 
-for i in range(5, len(dataarrest)):  # replace 20 with len(dataarrest)
+for i in range(5, len(dataarrest)):
     if dataarrest[i][1] == 'F':
         farrest.append(int(dataarrest[i][0]))
     elif dataarrest[i][1] == 'R':
@@ -48,20 +48,74 @@ print rarrest
 
 spire_entries = spireextract("mtb.txt")
 
-possiblestarts = defaultdict(list)
+possiblestartsgrow = defaultdict(list)
+possiblestartsarrest = defaultdict(list)
+spread = 200
+
 
 for key, value in spire_entries.items():
     matchat = re.search('\((\d*):(\d*)', key)
     matchstart = matchat.group(1)
     matchend = matchat.group(2)
+    matchloc = re.search('(\w*)\sof.*', value['Feature']).group(1)
     gene = re.search('term=(\w*)', value['URL']).group(1)
     geneloc = re.search('(\d*)\.\.(\d*)', value['Position'])
     genestart = int(geneloc.group(1))
     geneend = int(geneloc.group(2))
 
+    if matchloc == 'upstream':
+        if value['Direction'] == '+':
+            for TSS in fgrow:
+                if genestart - spread <= TSS <= genestart:
+                    possiblestartsgrow[gene].append(TSS)
+            for TSS in farrest:
+                if genestart - spread <= TSS <= genestart:
+                    possiblestartsarrest[gene].append(TSS)
+
+        if value['Direction'] == '-':
+            for TSS in fgrow:
+                if geneend <= TSS <= geneend + spread:
+                    possiblestartsgrow[gene].append(TSS)
+            for TSS in farrest:
+                if geneend <= TSS <= geneend + spread:
+                    possiblestartsarrest[gene].append(TSS)
+
+for kg, vg in sorted(possiblestartsgrow.items()):
+    if possiblestartsarrest.has_key(kg):
+        print kg, ' has start sites:', vg, ' during growth and ', possiblestartsarrest[kg], ' during arrest'
+
+# print possiblestartsarrest
+#print '\n\n'
+#print possiblestartsgrow
+
+'''
+    if matchloc == 'downstream':
+        if value['Direction'] == '+':
+            for TSS in fgrow, farrest:
+                if genestart - 200 <= TSS <= genestart:
+                    #and there are no TSS between the end of the gene and the start of the next IRE match, as well as no Start Codons
+                    possiblestartsgrow[gene].append(TSS)
+        if value['Direction'] == '-':
+            for TSS in fgrow, farrest:
+                if geneend <= TSS <= geneend + 200:
+                    possiblestartsgrow[gene].append(TSS)
+
+
+
+
+
+    if value['Direction'] == '-':
+        TSSlistgrow = rgrow
+        TSSlistarrest = rarrest
+    else:
+        TSSlistgrow = fgrow
+        TSSlistarrest = farrest
+
     if value['Direction'] == '+':
         for TSS in fgrow:
             if genestart - 200 <= TSS <= genestart:
-                possiblestarts[gene].append(TSS)
+                possiblestartsgrow[gene].append(TSS)
 
-print possiblestarts["Rv1073"]
+print possiblestartsgrow
+'''
+
